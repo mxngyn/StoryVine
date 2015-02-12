eff507383a20dd7a822b633f3a160929e7f7e726
alexjtark <alex.tarkowski@gmail.com>
2015-02-12 18:00:56 -0500

    add vote get and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :snippets, only: [:index, :new, :create, :show]
   resources :users, except: :index
+
+  get 'votes/show', to: 'votes#show'
+  post 'votes/create', to: 'votes#create'
 end
 
