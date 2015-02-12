cc5b4caba4428995e3eecca29e329b5b7c43283c
alexjtark <alex.tarkowski@gmail.com>
2015-02-12 17:22:56 -0500

    add initial test

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../spec_helper'
+
+describe Vote do
+  it "tests if true is true" do
+    expect(true).to eq true
+  end
+end
+
+describe 'Initial Test' do
+  it 'should test if true is true' do
+    expect(true).to eq true
+  end
+end
