f679cd0060fd6eae664d88431227111accc9d848
Mai & Pearl Shin <pair+mxngyn.pearlshin@devbootcamp.com>
2015-02-12 15:58:12 -0500

    Change user/author aliasing

--- db/migrate/20150212195133_create_stories.rb
+++ db/migrate/20150212195133_create_stories.rb
@@ -2,8 +2,8 @@ class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
       t.references :snippet
-      t.references :user, as: :author
       t.references :parent, index: true
+      t.integer :author_id
       t.string :title
       t.text :content
       t.boolean :published
