a8a92022b2ad2cbcc248fb93048f42f923c60140
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-12 17:40:36 -0500

    Make create controller for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      sesssion[:user_id] = @user.user_id
+      redirect_to user_show_path(@user)
+    else
+      redirect_to :back
+    end
   end
 
   def new
