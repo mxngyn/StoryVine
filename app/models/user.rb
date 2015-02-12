43ad8dfbe5d53899b108bca0923482440062c38b
Mai & Pearl Shin <pair+mxngyn.pearlshin@devbootcamp.com>
2015-02-12 15:59:03 -0500

    Add foreign_key to author_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :snippets
-  has_many :stories
+  has_many :stories, foreign_key: :author_id
   has_many :votes
 
   validates :username, presence: true
