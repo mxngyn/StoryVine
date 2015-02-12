c6143a11cedad50257f967d4c1898a8e97d8ceba
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-02-12 18:00:22 -0500

    Adjust routes so that signup path is explicit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :snippets, only: [:index, :new, :create, :show]
-  resources :users, except: :index
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  resources :users, only: [:show, :edit, :update, :delet]
 end
 
