752b93ce72e4be7baaf97a1508debe5439aecde7
alexjtark <alex.tarkowski@gmail.com>
2015-02-13 10:04:12 -0500

    add up and down vote methods to controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,28 +1,26 @@
 class VotesController < ApplicationController
 
-  def create
-    @story = Story.find(id)
-    @vote = Vote.new(story_id: @story.id, liked: params[:vote][:liked], user_id: current_user.id)
-    if @vote.save
-      redirect_to :back
-    else
-      redirect_to :back
+  def upvote
+    @story = Story.find(params[:id])
+    @vote = Vote.new(user_id: 1, story_id: @story.id, liked: true)
+    unless @vote.save
+      db_vote = Vote.where(user_id: 1, votable_id: @story.id)[0]
+      db_vote.update(liked: true)
     end
+    redirect_to :back
   end
 
-  def show
+  def downvote
+    @story = Story.find(params[:id])
+    @vote = Vote.new(user_id: 1, story_id: @story.id, liked: false)
+    unless @vote.save
+      db_vote = Vote.where(user_id: current_user.id, votable_id: @story.id)[0]
+      db_vote.update(liked: false)
+    end
+    redirect_to :back
   end
 
   private
-
-  def count_votes(story_votes)
-    votes = story_votes.map do |vote|
-      vote.liked
-    end
-    true_count = votes.count(true)
-    false_count = votes.count(false)
-    true_count - false_count
-  end
 
   def vote_params
     params.require(:vote).permit(:liked, :story_id, :user_id)
