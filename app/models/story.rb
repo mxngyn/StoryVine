a86506edbd1507e039011cdf9356c364fddadaa0
Mai <mxngyn@gmail.com>
2015-02-13 17:12:16 -0500

    Add class methods for most recent and most popular to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -12,6 +12,14 @@ class Story < ActiveRecord::Base
     self.votes.where(liked: true).count
   end
 
+  def self.most_popular
+    Story.all.map { |story| {story => story.vote_count} }.take(10)
+  end
+
+  def self.most_recent
+    Story.all.sort_by(&:created_at).reverse.take(10)
+  end
+
 end
 
 
