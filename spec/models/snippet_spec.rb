f3e1f356aa9661489c2d11c096159bb0aa707719
Mai <mxngyn@gmail.com>
2015-02-13 11:47:41 -0500

    Snippet model tests

---
+++ spec/models/snippet_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'factory_girl_rails'
+
+describe Snippet do
+
+  let(:snippet) { FactoryGirl.create(:snippet)}
+
+  it "has a valid FACTORY" do
+    expect(FactoryGirl.create(:snippet)).to be_valid
+  end
+
+  it "to be valid Snippet" do
+    expect(snippet).to be_valid
+  end
+
+  it "should have content" do
+    expect(snippet.content).to eq("THIS IS A SNIPPET, WHADDUP")
+  end
+
+  it "should have a user id" do
+    expect(snippet.user_id).to eq(1)
+  end
+
+end
