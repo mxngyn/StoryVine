5a552d95679a35c284147085e49302f8d0fce3d3
Mai <mxngyn@gmail.com>
2015-02-13 16:49:18 -0500

    Refactor autosave

--- app/assets/javascripts/autosave.js
+++ app/assets/javascripts/autosave.js
@@ -1,17 +1,25 @@
-$(document).ready(function (){
-  $(".edit_snippet").on("keyup", function(event){
-    var $target = $(event.target);
-    var $url = $('.edit_snippet')[0].action;
-    var $data = $('.edit_snippet').serialize();
-    $.ajax({
-      type: "PATCH",
-      url: $url,
-      data: $data,
-      dataType: "text"
-    }).done(function(response){
-      console.log(response);
-      $(".autosave").html(response);
-    });
-
+function autosave () {
+  var $url = $('.edit_snippet')[0].action;
+  var $data = $('.edit_snippet').serialize();
+  $.ajax({
+    type: "PATCH",
+    url: $url,
+    data: $data,
+    dataType: "text"
+  }).done(function(response){
+    console.log(response);
+    $(".autosave").html(response);
   });
+}
+
+
+$(document).ready(function (){
+  var autosaveOnFocus;
+  $('.redactor_editor').focus(function() {
+    autosaveOnFocus = setTimeout(autosave, 5000);
+  });
+
+  $('.redactor_editor').blur(function() {
+    clearTimeout(autosaveOnFocus);
+  })
 });
