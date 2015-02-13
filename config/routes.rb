d09b4a68cdfa15769000a042eea48fa2f8e06e51
Pearl Shin <pearlshin22@gmail.com>
2015-02-12 21:59:56 -0500

    add routing for users

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :stories
 
 
+  root "welcome#index"
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
