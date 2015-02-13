a1e0dfbf5da5fe6de2bc87d0486c9b6ec7f387be
alexjtark <alex.tarkowski@gmail.com>
2015-02-12 21:35:42 -0500

    work in progress for vote counter

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,11 @@
 class VotesController < ApplicationController
 
   def create
-    @vote = Vote.new(vote_params)
-    @vote.user = current_user
+    @story = Story.find(id)
+    @vote = Vote.new(story_id: @story.id, liked: params[:vote][:liked], user_id: current_user.id)
     if @vote.save
+      redirect_to :back
+    else
       redirect_to :back
     end
   end
@@ -13,7 +15,17 @@ class VotesController < ApplicationController
 
   private
 
+  def count_votes(story_votes)
+    votes = story_votes.map do |vote|
+      vote.liked
+    end
+    true_count = votes.count(true)
+    false_count = votes.count(false)
+    true_count - false_count
+  end
+
   def vote_params
-    params.require(:vote).permit(:liked)
+    params.require(:vote).permit(:liked, :story_id, :user_id)
   end
 end
+
