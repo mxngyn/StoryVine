26815af99a80981477ff549fb9b404fb689e1c6d
Alex Tarkowski <alex.tarkowski@gmail.com>
2015-02-13 20:17:47 -0500

    add patch route for vote

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   mount RedactorRails::Engine => '/redactor_rails'
   resources :snippets
 
-
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create', as: 'new_sign_up'
 
-  get 'votes/show', to: 'votes#show'
-  post 'votes/create', to: 'votes#create'
-
-  get 'stories/:id/upvote', to: 'votes#upvote', as: 'vote_up'
+  patch 'stories/:id/upvote', to: 'votes#upvote', as: 'vote_up'
   get 'stories/:id/downvote', to: 'votes#downvote', as: 'vote_down'
 
   resources :users, only: [:show, :edit, :delete]
