d015e83d4707e87d31cebd362f9f4833ee609a1e
Mai <mxngyn@gmail.com>
2015-02-15 13:36:54 -0500

    Delete migrations

--- db/migrate/20150215042728_add_flag_column_to_stories.rb
+++
@@ -1,5 +0,0 @@
-class AddFlagColumnToStories < ActiveRecord::Migration
-  def change
-    add_column :stories, :flagged, :boolean, :default => false
-  end
-end
--- db/migrate/20150215042734_add_flag_column_to_snippets.rb
+++
@@ -1,5 +0,0 @@
-class AddFlagColumnToSnippets < ActiveRecord::Migration
-  def change
-    add_column :snippets, :flagged, :boolean, :default => false
-  end
-end
