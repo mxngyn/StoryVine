4b60ba01a21d0d10bc956cbc27b284c404272f18
Pearl Shin <pearlshin22@gmail.com>
2015-02-14 19:35:31 -0500

    add badges migration table

--- db/migrate/20150214204502_create_badges.rb
+++ db/migrate/20150214204502_create_badges.rb
@@ -1,8 +1,10 @@
 class CreateBadges < ActiveRecord::Migration
   def change
     create_table :badges do |t|
-      t.references :user
       t.string :title
+      t.references :story
+
+      t.timestamps
     end
   end
 end
