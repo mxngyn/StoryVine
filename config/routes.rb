b4a39cc6edc9d17525c20b4a49eda72a1faf3a87
Mai <mxngyn@gmail.com>
2015-02-15 13:36:45 -0500

    Add patch routes for stories and snippets

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+
+  patch 'snippets/:id/flag', to: 'snippets#flag', as: 'snippet_flag'
+  patch 'stories/:id/flag', to: 'stories#flag', as: 'story_flag'
 end
 
