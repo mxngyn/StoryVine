15b866789337621d5abb3e96809e84823c98d6f3
Mai <mxngyn@gmail.com>
2015-02-14 23:53:26 -0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212204219) do
+ActiveRecord::Schema.define(version: 20150215042734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150212204219) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "flagged",    default: false
   end
 
   create_table "stories", force: :cascade do |t|
@@ -48,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150212204219) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "flagged",    default: false
   end
 
   add_index "stories", ["parent_id"], name: "index_stories_on_parent_id", using: :btree
@@ -77,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150212204219) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
   create_table "votes", force: :cascade do |t|
