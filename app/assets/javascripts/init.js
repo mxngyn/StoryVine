f0fb78cf9251cb791d0e16aa735af893eb7156a1
Mai <mxngyn@gmail.com>
2015-02-16 18:38:38 -0500

    indexPage function

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -14,3 +14,19 @@ var warnBeforePublish = function() {
     $('#publish-warning').show();
   })
 }
+
+
+var indexPage = function() {
+  var fullPage = document.getElementById('fullpage');
+  if(fullPage) {
+    $('#fullpage').fullpage({
+      menu: true,
+      anchors:['firstSlide', 'secondSlide'],
+      navigation: true,
+      navigationPosition: 'right',
+      navigationTooltips: ['firstSlide', 'secondSlide'],
+      slidesNavigation: true,
+      slidesNavPosition: 'bottom',
+    });
+  }
+}
