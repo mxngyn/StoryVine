a46b7b7982a99912232cf3df8fb5d93f035e2869
Mai <mxngyn@gmail.com>
2015-02-16 20:44:52 -0500

    Vote up and vote down ajax calls

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
   SaveWidget.autoSaveContent();
   warnBeforePublish();
   indexPage();
-
+  voteUpdate();
+  voteDown();
 });
 
 
@@ -30,3 +31,48 @@ var indexPage = function() {
     });
   }
 }
+
+
+var voteUpdate = function() {
+  var voteFormExists = document.getElementsByClassName('edit_story')[0];
+
+  if (voteFormExists) {
+    $('.edit_story').on('submit', function(event) {
+      event.preventDefault();
+      var $target = $(event.target);
+      $target.hide();
+      var $url = $('.edit_story')[0].action;
+      $.ajax({
+        type: "PATCH",
+        url: $url,
+        dataType: "text"
+      }).done(function(response) {
+        $('.vote-count').html(response);
+      });
+    });
+  }
+}
+
+var voteDown = function() {
+  var downVoteButton = document.getElementsByClassName('button_to')[0];
+  if (downVoteButton) {
+    console.log("hello");
+    $('.button_to').on('submit', function(event) {
+      event.preventDefault();
+      var $target = $(event.target);
+      $target.hide();
+      var $url = $('.button_to')[0].action;
+      $.ajax({
+        type: "DELETE",
+        url: $url,
+        dataType: "text"
+      }).done(function(response) {
+        $('.vote-count').html(response);
+      });
+    });
+  }
+}
+
+
+
+
